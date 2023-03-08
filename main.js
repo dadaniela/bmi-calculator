5268ffcf900e4ec1c5152cc9270ccf1ec8df94bb
Daniela <trindadedv@gmail.com>
2023-03-08 17:41:45 +0100

    handling error message

--- main.js
+++ main.js
@@ -8,10 +8,22 @@ form.onsubmit = function(event) {
     event.preventDefault();
     const weight = inputWeight.value;
     const height = inputHeight.value;
+    const showAlertError = notNumber(weight) || notNumber(height);
+    
+    if(showAlertError) {
+        console.log("mostrar erro");
+        return;
+    };
+
     const bmiResult = bmiCalculator(weight, height);
     const message = `Your BMI is ${bmiResult}`;
     Modal.open();
     Modal.message.innerText = message;
+};
+
+function notNumber (value) {
+    return isNaN(value) || value == ""
+    
 };
 
 function bmiCalculator(weight, height) {
